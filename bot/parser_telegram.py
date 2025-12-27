5c7721f8696660edab9e5501c8f7b49af09eb456
TimurShlyonsky <timur.shlyonsky@gmail.com>
2025-12-27 18:34:03 +0200

    Update parser_telegram.py

--- bot/parser_telegram.py
+++ bot/parser_telegram.py
@@ -1,15 +1,21 @@
 import os
+import asyncio
 from telethon import TelegramClient
 
 API_ID = int(os.getenv("TG_API_ID"))
 API_HASH = os.getenv("TG_API_HASH")
 CHANNEL = os.getenv("TG_CHANNEL")  # без @
 
-def test_read_channel(limit: int = 5):
-    with TelegramClient("promo_session", API_ID, API_HASH) as client:
-        channel = client.get_entity(CHANNEL)
 
-        for message in client.iter_messages(channel, limit=limit):
+async def test_read_channel(limit: int = 5):
+    async with TelegramClient("promo_session", API_ID, API_HASH) as client:
+        channel = await client.get_entity(CHANNEL)
+
+        async for message in client.iter_messages(channel, limit=limit):
             if message.text:
                 print("-----")
                 print(message.text)
+
+
+def run_test():
+    asyncio.run(test_read_channel())
